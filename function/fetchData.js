eebe138ef26df98fe17c55a20cb71f8ff24b68d5
kaif-0 <kaif8700979251@gmail.com>
2025-04-11 15:26:27 +0530

    commit

--- function/fetchData.js
+++ function/fetchData.js
@@ -8,27 +8,6 @@ config();
 
 const GOOGLE_SHEET_ID = process.env.NSE_GOOGLE_SHEET_ID;
 
-function waitForFileToDownload(filePath, timeout = 30000) {
-  return new Promise((resolve, reject) => {
-    const startTime = Date.now();
-
-    const interval = setInterval(() => {
-      if (fs.existsSync(filePath)) {
-        const stats = fs.statSync(filePath);
-        if (stats.size > 0) {
-          clearInterval(interval);
-          resolve();
-        }
-      }
-
-      if (Date.now() - startTime > timeout) {
-        clearInterval(interval);
-        reject(new Error("Download timeout: File was not saved."));
-      }
-    }, 500); 
-  });
-}
-
 export const fetchData = async (urls) => {
   try {
     for (const singleUrlData of urls) {
@@ -45,12 +24,14 @@ export const fetchData = async (urls) => {
 
       const page = await browser.newPage();
 
+      //alllow download in download folder
       const downloadPath = path.resolve("./downloads");
       await page._client().send("Page.setDownloadBehavior", {
         behavior: "allow",
         downloadPath: downloadPath,
       });
 
+      //bypassing bot detection
       await page.setUserAgent(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
       );
@@ -60,31 +41,36 @@ export const fetchData = async (urls) => {
       });
 
       const { url, buttonId, fileName, sheetName } = singleUrlData;
-      const filePath = path.join(downloadPath, fileName);
-
       await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000 });
 
-      await page.waitForSelector(buttonId, { timeout: 60000 });
+      await page.waitForSelector(`${buttonId}`, { timeout: 60000 });
 
-      if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
-      }
-
+      //download URL
       const csvUrl = await page.evaluate((btnId) => {
-        const csvLink = document.querySelector(btnId);
+        const csvLink = document.querySelector(`${btnId}`);
         return csvLink ? csvLink.href : null;
       }, buttonId);
 
       if (csvUrl) {
         console.log(`Downloading CSV from: ${csvUrl}`);
 
-        await page.click(buttonId);
-        console.log("Waiting for download to complete...");
+        //click on download button
+        await page.click(`${buttonId}`);
 
-        await waitForFileToDownload(filePath);
-        console.log("Download complete. Uploading to Google Sheets...");
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, 5000);
+        });
+
+        // const path = `./downloads/Advance.csv`;
 
         await uploadCSVToGoogleSheet(fileName, GOOGLE_SHEET_ID, sheetName);
+
+        //copy the csv file to the data.txt file
+        // const csvContent = fs.readFileSync(`./downloads/${fileName}`, "utf-8");
+        // console.log(csvContent);
+        // fs.appendFileSync("./data.txt", `Data from ${url}:\n${csvContent}\n\n`, "utf-8");
       }
 
       await browser.close();
@@ -92,7 +78,7 @@ export const fetchData = async (urls) => {
   } catch (error) {
     if (error.message.includes("EAGAIN")) {
       console.log("Restarting process due to EAGAIN error...");
-      process.exit(1); 
+      process.exit(1);
     }
     throw new Error(`Error fetching CSV data: ${error.message}`);
   }
